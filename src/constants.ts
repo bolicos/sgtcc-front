a07c387aac06c3bf8d9b36dc045a57ac119ec79a
Ana Lucia Bolico <51336956+analuciabolico@users.noreply.github.com>
2021-08-15 23:21:55 -0300

    adjust constant api_auth_url

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ export const WEB_APP = {
   ENVIRONMENT: process.env.REACT_APP_ENV,
   BASE_URL: process.env.REACT_APP_BASE_URL,
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
-  API_AUTH_URL: process.env.REACT_APP_BFF_BASE_URL,
+  API_AUTH_URL: process.env.REACT_APP_API_AUTH_URL,
 };
 
 export const ROUTES = {
