22d8d75688a0c817bf17fc1b16dbede20bb15f2e
anakettl <anakettl@gmail.com>
2021-08-19 19:38:44 -0300

    feat - create dashboard to student

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,90 @@
 import React from 'react'
-// import { Row, Col, Container, Button } from "react-bootstrap"
-import { Container } from "react-bootstrap"
+import { Container, Card, CardGroup, Button } from "react-bootstrap"
 import PageHeader from "#/components/PageHeader";
-// import { ROUTES } from "#/constants"
+import css from "./styles.module.scss";
+import { ROUTES } from "#/constants"
 
 export const Dashboard: React.FC = () => {
   return (
-    <Container>
+    <Container className={css["container-body"]}>
       <PageHeader title="Dashboard"/>
 
+      {/* <CardGroup>
+        <Card className={css["card"]}>
+          <Card.Body className={css["card-body"]}>
+            <Card.Title>Alunos</Card.Title>
+            <Card.Text>
+              Entre em contato com seus colegas
+            </Card.Text>
+          </Card.Body>
+
+          <div className="d-grid gap-2">
+            <Button href="" className={css["button-large"]} variant="success" size="lg">
+              {'>'}
+            </Button>
+          </div>
+
+        </Card>
+        <Card className={css["card"]}>
+          <Card.Body className={css["card-body"]}>
+            <Card.Title>Professor</Card.Title>
+            <Card.Text>
+              Converse com seu professor
+            </Card.Text>
+          </Card.Body>
+
+          <div className="d-grid gap-2">
+            <Button className={css["button-large"]} variant="success" size="lg">
+              {'>'}
+            </Button>
+          </div>
+        </Card>
+      </CardGroup> */}
+      <div className={css["div"]}></div>
+      <CardGroup >
+        <Card className={css["card"]}>
+          <Card.Body className={css["card-body"]}>
+            <Card.Title>Propostas</Card.Title>
+            <Card.Text>
+              Gerencie suas propostas
+            </Card.Text>
+          </Card.Body>
+
+          <div className="d-grid gap-2">
+            <Button href={ROUTES.PROPOSAL_CREATE()} className={css["button-large"]} variant="success" size="lg">
+              {'>'}
+            </Button>
+          </div>
+        </Card>
+        <Card className={css["card"]}>
+          <Card.Body className={css["card-body"]}>
+            <Card.Title>Avaliações</Card.Title>
+            <Card.Text>
+              Veja suas notas
+            </Card.Text>
+          </Card.Body>
+
+          <div className="d-grid gap-2">
+            <Button href={ROUTES.EXAMINATION_CREATE()} className={css["button-large"]} variant="success" size="lg">
+              {'>'}
+            </Button>
+          </div>
+        </Card>
+        <Card className={css["card"]}>
+          <Card.Body className={css["card-body"]}>
+            <Card.Title>Bancas</Card.Title>
+            <Card.Text>
+              Acompanhe a data da sua banca
+            </Card.Text>
+          </Card.Body>
+
+          <div className="d-grid gap-2">
+            <Button href={ROUTES.BOARD_CREATE()} className={css["button-large"]} variant="success" size="lg">
+              {'>'}
+            </Button>
+          </div>
+        </Card>
+      </CardGroup>
     </Container>
   )
 }
